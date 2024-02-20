5a92ff5b517d160788cf2840fb83a1f5e2b3ca73
debrajray061 <160575979+debrajray061@users.noreply.github.com>
2024-02-20 12:14:31 +0530

    Add files via upload

---
+++ P3/P3_4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void) {
+
+printf(" Name                        : DEBRAJ RAY                       \n");
+printf(" Age                         : 18                               \n");
+printf(" Gender                      : Male                             \n");
+printf("              QUALIFICATION                                     \n");
+printf(" Secondary percentage        : 94.5%                            \n");
+printf(" Higher secondary percentage : 77.7%                            \n");
+printf(" Current Qualification status: Pursuing B.Tech 1st Year At KIIT \n");
+printf(" Branch                      : CSE       \n");
+printf(" Subjects: T&N.M   EVS    ED & Graphics   Programming   Physics   Sc.L.S. \n");
+
+return 0;
+}
+
