f856f71106ff5ca9af8cba97ef8d61ab3695e930
debrajray061 <160575979+debrajray061@users.noreply.github.com>
2024-03-12 12:44:20 +0530

    Add files via upload

---
+++ P10/P10_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()	
+{
+
+	int n,i,j;
+	printf("Enter the no of rows");
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			printf("%d",i);
+		}
+	printf("\n");
+	}
+	}	
+		
+	
+	
